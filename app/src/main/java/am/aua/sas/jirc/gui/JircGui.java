1e614043346b98406fe826b32e092fe9c7b6a4e8
Samvel Davtyan <samvelxdavtyan@gmail.com>
2022-05-09 20:16:36 +0400

    feat: add connection logic

--- app/src/main/java/am/aua/sas/jirc/gui/JircGui.java
+++ app/src/main/java/am/aua/sas/jirc/gui/JircGui.java
@@ -1,8 +1,12 @@
 package am.aua.sas.jirc.gui;
 
+import am.aua.sas.jirc.irc.IRCClient;
+
 import javax.swing.*;
 
 public class JircGui {
+    public static IRCClient client;
+
     public JircGui() {
         ConnectionForm loginForm = new ConnectionForm();
         show(loginForm);
@@ -11,5 +15,10 @@ public class JircGui {
     protected static void show(JFrame frame) {
         frame.pack();
         frame.setVisible(true);
+    }
+
+    protected static void hide(JFrame frame) {
+        frame.setVisible(false);
+        frame.dispose();
     }
 }
