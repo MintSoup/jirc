baf50379c1902cc2a7d1817fe0ff897b1d5562ca
Samvel Davtyan <samvelxdavtyan@gmail.com>
2022-05-09 17:17:03 +0400

    feat: add strings class

---
+++ app/src/main/java/am/aua/sas/jirc/Strings.java
@@ -0,0 +1,7 @@
+package am.aua.sas.jirc;
+
+public final class Strings {
+    public static final String SERVER = "Server";
+    public static final String NICKNAME = "Nickname";
+    public static final String USERNAME = "Username";
+}
